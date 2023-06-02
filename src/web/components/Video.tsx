d556f98901859eb2011a104b5f27c7ebce162b0d
UEMOTO Soichiro <uemoto.soichiro@ryobi.co.jp>
2023-06-02 17:13:30 +0900

    feat: ROIの描画処理を追加

--- src/web/components/Video.tsx
+++ src/web/components/Video.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useRef, useState } from 'react';
 
+interface Rectangle {
+  startX: number;
+  startY: number;
+  width: number;
+  height: number;
+}
+
 export const Video = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   let videTracks: MediaStreamTrack[] = [];
   let videoTrack: MediaStreamTrack | null = null;
   let animationFrameId: number | null = null;
   const [isCapturing, setIsCapturing] = useState(false);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [rectangle, setRectangle] = useState<Rectangle | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -27,6 +36,15 @@ export const Video = () => {
               canvas.width = imageBitmap.width;
               canvas.height = imageBitmap.height;
               ctx.drawImage(imageBitmap, 0, 0);
+              if (rectangle) {
+                ctx.strokeStyle = 'red';
+                ctx.strokeRect(
+                  rectangle.startX,
+                  rectangle.startY,
+                  rectangle.width,
+                  rectangle.height
+                );
+              }
             })
             .catch((err) => {
               console.log('Something went wrong!', err);
@@ -43,7 +61,7 @@ export const Video = () => {
         cancelAnimationFrame(animationFrameId);
       }
     };
-  }, [isCapturing]);
+  }, [isCapturing, rectangle]);
 
   const handleStart = () => {
     setIsCapturing(true);
@@ -52,9 +70,40 @@ export const Video = () => {
   const handleStop = () => {
     setIsCapturing(false);
   };
+  const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    setIsDrawing(true);
+    const rect = e.currentTarget.getBoundingClientRect();
+    setRectangle({
+      startX: e.clientX - rect.left,
+      startY: e.clientY - rect.top,
+      width: 0,
+      height: 0,
+    });
+  };
+
+  const handleMouseUp = () => {
+    setIsDrawing(false);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing || !rectangle) return;
+    const rect = e.currentTarget.getBoundingClientRect();
+    setRectangle({
+      ...rectangle,
+      width: e.clientX - rect.left - rectangle.startX,
+      height: e.clientY - rect.top - rectangle.startY,
+    });
+  };
+
   return (
     <>
-      <canvas id="canvas" ref={canvasRef}></canvas>
+      <canvas
+        id="canvas"
+        ref={canvasRef}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      ></canvas>
       <button onClick={handleStart}>Start</button>
       <button onClick={handleStop}>Stop</button>
       <div id="graph"></div>
